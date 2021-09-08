fc825a20e0b9a3c8adebba594cc470d1761c13db
Ryder Cook <ryder3222@gmail.com>
2021-09-08 11:58:26 -0500

    Week 2 Assignment

---
+++ main.js
@@ -0,0 +1,14 @@
+const string1 = "A string primitive";
+const string2 = 'Also a string primitive';
+const string3 = `Yet another string primitive`;
+
+console.log(string1.length);
+
+//charAt()
+console.log(string1.charAt(0));
+
+//concat()
+console.log(string1 + string2);
+
+//endWidth()
+console.log(string1.endWidth("ive"));
