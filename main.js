5e9db4a99561ab29183965299720a60945bd2d60
Ryder Cook <ryder3222@gmail.com>
2021-09-16 12:13:01 -0500

    95930294_Assignment_1

--- main.js
+++ main.js
@@ -1,14 +1,59 @@
-const string1 = "A string primitive";
-const string2 = 'Also a string primitive';
-const string3 = `Yet another string primitive`;
+// Ryder Cook
+// INF651 - Front End Web Development I
+// 14 September 2021
 
-console.log(string1.length);
+// Practice Assignment 1
 
-//charAt()
-console.log(string1.charAt(0));
+// Question 1
+var myNumber;
 
-//concat()
-console.log(string1 + string2);
+// Question 2
+typeof myNumber;
+myNumber = 5;
 
-//endWidth()
-console.log(string1.endWidth("ive"));
+// Question 3
+var myOtherNumber;
+typeof myOtherNumber;
+myOtherNumber = 5;
+
+// Question 4
+var myTotal;
+typeof myTotal;
+myTotal = myNumber * myOtherNumber;
+console.log("The product is " + myTotal);
+
+// Question 5
+var myName;
+typeof 'myName';
+var myCombo;
+typeof 'myCombo';
+myCombo = myNumber + myOtherNumber;
+console.log("The sum is " + myCombo);
+
+// Question 6
+var myOtherCombo;
+typeof myOtherCombo;
+myOtherCombo = myNumber + myOtherNumber;
+console.log("The sum is " + myOtherCombo);
+
+// Question 7
+var myRemainder;
+typeof myRemainder;
+myRemainder = myNumber / 5;
+console.log("The remainder is " + myRemainder);
+
+// Question 8
+var i;
+typeof i;
+i = 2;
+console.log("i = " + i);
+
+// Question 9
+var int;
+typeof int;
+int = 4;
+console.log("int = " + int);
+
+// Question 10
+var myBoolean = new Boolean(false);
+console.log("myBoolean = " + myBoolean.toString());
